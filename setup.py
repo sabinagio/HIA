d4818e1d7aef0e242341e90ab20f6f56c491a43c
Sabina Firtala <sabina@frontline100.com>
2025-01-31 21:51:07 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Helpful Information as Aid - a Hackathon for Good 7 project - February 2024.',
+    author='R-accoon Squared',
+    license='',
+)

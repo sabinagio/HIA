001caff459972b9d72cc12dfce83ca5ae2168e7f
camillecoeurjoly <camillecoeurjoly@gmail.com>
2025-02-01 22:03:28 +0100

    web agent prototype update with Command output to response quality agent

--- src/agents/web_agent.py
+++ src/agents/web_agent.py
@@ -2,16 +2,22 @@ from dotenv import load_dotenv
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from langchain_anthropic import ChatAnthropic
+from langgraph.types import Command
 
 load_dotenv()
 
+llm = ChatAnthropic(
+    model="claude-3-5-haiku-20241022",
+    temperature=0
+)
+
 def web_search(query: str) -> str:
-    wrapper = DuckDuckGoSearchAPIWrapper(region="nl-nl", max_results=2)
+    wrapper = DuckDuckGoSearchAPIWrapper(region="nl-nl", max_results=2) # time='y' limit to past year (m, d, w)
     search_tool = DuckDuckGoSearchResults(api_wrapper=wrapper)
     results = search_tool.run(query)
     return results
 
-def web_agent(query_context: dict) -> dict:
+def prompt_search(query_context: dict) -> dict:
     """
     Uses query understanding output to perform targeted web searches.
 
@@ -23,11 +29,6 @@ def web_agent(query_context: dict) -> dict:
             "language": str        # e.g. "english"
         }
     """
-
-    llm = ChatAnthropic(
-        model="claude-3-5-haiku-20241022",
-        temperature=0
-    )
 
     # Map domains to relevant websites - otherwise could get things from newspapers "the red cross did this last year"
     domain_sites = {
@@ -86,6 +87,50 @@ def web_agent(query_context: dict) -> dict:
     results = web_search(full_query)
 
     return {"web_response": results}
+
+def search_summary(query_context: dict, search_result: dict) -> dict:
+
+    system_prompt = """
+    You are a Red Cross assistant who helps people in need in the Netherlands.
+    
+    Information given:
+        - question(s) the vulnerable person is asking
+        - relevant search results
+        - vulnerable person's preferred language from question and context
+        - Optional: vulnerable person's city
+    
+    Based on that information, you need to summarise the search results to address
+    the vulnerable person's immediate need.
+    Use kind and inclusive language, respecting the person's dignity.
+    Give an answer that is as clear and useful as possible, the vulnerable person
+    is in a stressful situation and needs to be helped and reassured.
+    Answer in the preferred language of the vulnerable person.
+    Include links to the source articles in your answer to foster trust.
+    """
+
+    llm_response = llm.invoke([
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": f"""
+            Original query: {query_context['original_query']}
+            Location: {query_context['entities'].get('location', 'Netherlands')}
+            Domains: {query_context['domains']}
+            Language: {query_context['language']}
+        """}
+    ])
+
+    response = llm_response.content
+
+    return {"web_agent_response": response}
+
+def web_agent(query_context: dict):
+    search_result = prompt_search(query_context)
+    web_agent_response = search_summary(query_context, search_result)
+    return Command(
+        goto="response_quality",
+        update={
+            "initial_response": web_agent_response
+        }
+    )
 
 if __name__ == "__main__":
     test_context = {
